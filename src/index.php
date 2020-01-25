99b9e954feecc557529a06a2f2c659897e120eca
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2020-01-25 18:04:56 +0100

    initial commit

---
+++ src/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Local CSS -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- Font -->
+	<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Monoton|Righteous|Sacramento&display=swap" rel="stylesheet">
+	<title>Fabrika &bull; Home</title>
+	<meta name="description" content=""/>
+    <meta name="keywords" content="">
+</head>
+<body>
+	<!-- NAVIGATION -->
+	<!--nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="index.html"><img class="logo-img" src="img/Logos/logo1.png"></a>
+			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="#home-head" class="nav-link">Home</a>
+					</li>
+					<li class="nav-item">
+						<a href="#skills-head" class="nav-link">Skills</a>
+					</li>
+					<li class="nav-item">
+						<a href="#projects-head" class="nav-link">Projects</a>
+					</li>
+					<li class="nav-item">
+						<a href="#about-head" class="nav-link">About</a>
+					</li>
+					<li class="nav-item">
+						<a href="#contact-head" class="nav-link">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav-->
+	<nav>
+		<div class="container">
+			<h1 class="">FABRIKA</h1>	
+		</div>
+	</nav>
+	<a href="gallery/gallery.html"><h2 class="text-primary">Gallery</h2></a>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!-- Font CDN -->
+	<!-- JS not Supported! -->
+	<noscript>Your browser does not support JavaScript!</noscript>
+</body>
+</html>
