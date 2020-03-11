2f3fe9108dd367a9912cdb6cc8304eea45aef1bd
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2020-03-11 17:54:00 +0100

    added mailing support

--- src/assets/logic/main.php
+++ src/assets/logic/main.php
@@ -1,4 +1,47 @@
 <?php 
+// Load Composer's autoloader
+require_once '../vendor/autoload.php';
+require 'config.php';
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Instantiation and passing `true` enables exceptions
+function send_mail($from = 'null', $name = 'null', $body = '. . .')
+{
+
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.sendgrid.net';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'apikey';                               // SMTP username
+        $mail->Password   = API_Key['SendGrid'];          			// SMTP password // API keys kept at config.php
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('noreply@fabrika.rs');
+        $mail->addAddress(email);                                   // Name is optional
+        $mail->addReplyTo($from, $name);
+
+        // Content
+        $mail->isHTML(true);                                        // Set email format to HTML
+        $title = $name.', '.$from.' sent from fabrika.rs';
+        $mail->Subject = $title;
+        $mail->Body    = $body;
+
+        $mail->send();
+        echo "<br><br><div class='alert alert-success alert-dismissible fade show' role='alert'>Message has been sent</div>"; # <button type='button' class='close' data-dismiss='alert' aria-label='Close' style='color: #155724 !important; box-shadow: none;'><span aria-hidden='true'>&times;</span></button>
+    } catch (Exception $e) {
+        echo "<br><br><div class='alert alert-danger alert-dismissible fade show' role='alert'>Message could not be sent. Mailer Error: {$mail->ErrorInfo}</div>"; # <button type='button' class='close' data-dismiss='alert' aria-label='Close' style='box-shadow: none;'><span aria-hidden='true'>&times;</span></button>
+    }
+}
+
 function WriteHead($a)
 	{
 		$location 	= '-1';
@@ -125,6 +168,8 @@ function WriteFooter($location = '')
 							<li><input type="submit" value="Send" class="primary bold"/></li>
 							<li><input type="reset" value="Reset" class="bold" /></li>
 						</ul>
+						<?php if (isset($_POST["email"]) && isset($_POST["name"]) && isset($_POST["message"])) 
+								{send_mail($_POST["email"], $_POST["name"], $_POST["message"]);} ?>
 					</form>
 				</section>
 			</div>
@@ -257,25 +302,24 @@ function WriteCard($img, $alt, $title, $subtitle, $body, $link, $href)
 	<?php 
 	}
 
-	function WriteRotate($img, $title, $text, $href)
-		{ 
+function WriteRotate($img, $title, $text, $href)
+	{ 
 
-		?>
-		<div class="col-12 col-sm-6 col-xl-4 row">
-			<div class="r-card">
-				<div class="r-front shadow-lg">
-					<img src="<?= $img; ?>" class="r-card-img" alt="...">
-				</div>
-				<div class="r-back shadow-lg">
-					<div class="r-backCont">
-						<h2><?= $title; ?></h2>
-						<p><?= $text; ?></p>
-						<a href="<?= ($href === '') ? '#' : $href ?>">More...</a>
-					</div>
+	?>
+	<div class="col-12 col-sm-6 col-xl-4 row">
+		<div class="r-card">
+			<div class="r-front shadow-lg">
+				<img src="<?= $img; ?>" class="r-card-img" alt="...">
+			</div>
+			<div class="r-back shadow-lg">
+				<div class="r-backCont">
+					<h2><?= $title; ?></h2>
+					<p><?= $text; ?></p>
+					<a href="<?= ($href === '') ? '#' : $href ?>">More...</a>
 				</div>
 			</div>
 		</div>
-		<?php
-		}
-
- ?>
+	</div>
+	<?php
+	}
+?>
